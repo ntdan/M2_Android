e783247fffa1d86009d91d6b86996f05d1703131
Ngô Tường Dân <ntdan@users.noreply.github.com>
2017-06-14 18:39:01 +0700

    initial

---
+++ MapDemo/app/src/main/java/vn/cusc/mapdemo/MapsActivity.java
@@ -0,0 +1,37 @@
+package vn.cusc.mapdemo;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng cantho = new LatLng(10.0359302, 105.7766715);
+        mMap.addMarker(new MarkerOptions().position(cantho).title("Marker in Can tho"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(cantho));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(cantho, 8));
+    }
+}
