00f410a7fe37f52e80171db5141c194aeb7de4dc
Ngô Tường Dân <ntdan@users.noreply.github.com>
2017-06-14 19:32:34 +0700

    initial

--- MapDemo/app/src/main/java/vn/cusc/mapdemo/MapsActivity.java
+++ MapDemo/app/src/main/java/vn/cusc/mapdemo/MapsActivity.java
@@ -3,16 +3,22 @@ package vn.cusc.mapdemo;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private ArrayList<MarkerOptions> markers = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +36,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng cantho = new LatLng(10.0359302, 105.7766715);
-        mMap.addMarker(new MarkerOptions().position(cantho).title("Marker in Can tho"));
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(cantho));
+        MarkerOptions marker = new MarkerOptions().position(cantho)
+                .title("Marker in Can tho")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.vn));
+
+        mMap.addMarker(marker);
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(cantho, 8));
+
+        markers.add(marker);
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                MarkerOptions markerOptions = new MarkerOptions().position(latLng)
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.vn));
+
+                markers.add(markerOptions);
+
+                mMap.addMarker(markerOptions);
+
+                LatLngBounds.Builder builder = new LatLngBounds.Builder();
+
+                for (int i = 0; i < markers.size(); i++) {
+                    builder.include(markers.get(i).getPosition());
+                }
+                LatLngBounds bounds = builder.build();
+                CameraUpdate camera = CameraUpdateFactory.newLatLngBounds(bounds, 10);
+                mMap.animateCamera(camera);
+            }
+        });
     }
 }
